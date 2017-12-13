c9d1428246626fa4ee7e31bb399eef1bd12e8087
Yuriy Netesov <yuriy.netesov@gmail.com>
2017-12-13 21:31:19 +0100

    fix tests

--- src/test/scala/learnfp/applicative/DisjunctionTest.scala
+++ src/test/scala/learnfp/applicative/DisjunctionTest.scala
@@ -11,11 +11,11 @@ class DisjunctionTest extends WordSpecLike with Matchers {
     def rght(x:Int):Disjunction[String, Int] = right[String, Int](x);
 
     "work with right" in {
-      {(x:Int, y:Int, z:Int) => x + y + z}.curried `<$>` rght(10) <*> rght(20) <*> rght(30)
+      {(x:Int, y:Int, z:Int) => x + y + z}.curried `<$>` rght(10) <*> rght(20) <*> rght(30) shouldBe rght(10 + 20 + 30)
     }
 
     "work with left" in {
-      {(x:Int, y:Int, z:Int) => x + y + z}.curried `<$>` rght(10) <*> left[String, Int]("boom") <*> rght(30)
+      {(x:Int, y:Int, z:Int) => x + y + z}.curried `<$>` rght(10) <*> left[String, Int]("boom") <*> rght(30) shouldBe left[String, Int]("boom")
     }
   }
 }
