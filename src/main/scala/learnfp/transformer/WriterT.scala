7dfa1c872e1c5bf1e7c3bd188fcef5c8547c935f
Yuriy Netesov <yuriy.netesov@gmail.com>
2017-12-21 18:14:26 +0100

    writerT

--- src/main/scala/learnfp/transformer/WriterT.scala
+++ src/main/scala/learnfp/transformer/WriterT.scala
@@ -1,13 +1,12 @@
 package learnfp.transformer
 
 
-import learnfp.functor.{Functor, Writer}
+import learnfp.functor.{Functor, FunctorOps, Writer}
 import learnfp.functor.FunctorOps._
-import learnfp.monad.Monad
+import learnfp.monad.{Monad, MonadOps}
 import learnfp.monad.MonadOps._
 import learnfp.monoid.Monoid
 import learnfp.monoid.MonoidOps._
-
 import learnfp.functor.WriterInstance._
 import learnfp.monad.WriterInstance._
 
@@ -17,33 +16,48 @@ object WriterT {
   implicit def writerTFunctorInstance[W, M[_]](implicit f:Functor[M], m:Monad[M], w:Monoid[W]) =
     new Functor[({type E[X] = WriterT[X, M, W]})#E] {
       override def fmap[A, B](a: WriterT[A, M, W])(fx: A => B): WriterT[B, M, W] = WriterT {
-        a.runWriterT fmap { av:Writer[W, A] =>
+        f.fmap(a.runWriterT){ av:Writer[W, A] =>
           av fmap {avv => fx(avv)}
         }
       }
     }
 
-  // TODO: to functor ops
+  implicit def toFunctorOps[A, M[_], W](a:WriterT[A, M, W])(implicit f:Functor[M], m:Monad[M], w:Monoid[W]):FunctorOps[A, ({type E[X] = WriterT[X, M, W]})#E] =
+    new FunctorOps[A, ({type E[X] = WriterT[X, M, W]})#E](a)
+
+
   implicit def writerTMonadInstance[W, M[_]](implicit f:Functor[M], m:Monad[M], w:Monoid[W]) =
     new Monad[({type E[X] = WriterT[X, M, W]})#E]() {
       override def pure[A](a: A): WriterT[A, M, W] = WriterT { m.pure(writerMonadInstance.pure(a)) }
       override def flatMap[A, B](a: WriterT[A, M, W])(fx: A => WriterT[B, M, W]): WriterT[B, M, W] = WriterT {
-        a.runWriterT >>= {
+          m.flatMap(a.runWriterT) {
           av:Writer[W, A] =>
             val (aw, avv) = av.run()
-            fx(avv).runWriterT fmap { bw =>
+            f.fmap(fx(avv).runWriterT) { bw =>
               val (bww, bv) = bw.run()
               Writer[W, B] {() => (bw.monoid.mappend(aw, bww), bv)} }
         }
       }
     }
 
-  def lift[A, W, M[_]](am:M[A])(implicit f:Functor[M], m:Monad[M], w:Monoid[W]):WriterT[A, M, W] = WriterT {
-    am fmap { a => writerMonadInstance[W].pure(a)}
+  def lift[A,M[_], W](am:M[A])(implicit f:Functor[M], m:Monad[M], w:Monoid[W]):WriterT[A, M, W] = WriterT {
+    f.fmap(am) { a => writerMonadInstance[W].pure(a)}
   }
 
-  // TODO: to monad ops
-  // TODO: lift
-  // TODO: pure
-  // TODO: tell
+  implicit def writerTToMonadOps[A, M[_], W](a:WriterT[A, M, W])(implicit f:Functor[M], m:Monad[M], w:Monoid[W]) =
+    new MonadOps[A, ({type E[X] = WriterT[X, M, W]})#E](a)
+
+
+  def pure[A, M[_], W](a:A)(implicit f:Functor[M], m:Monad[M], w:Monoid[W]) = WriterT {
+     m.pure(writerMonadInstance.pure(a))
+  }
+
+  implicit def writerTMonadTransInstance[A, M[_], W](a:A)(implicit f:Functor[M], m:Monad[M], w:Monoid[W]) =
+    new MonadTransformer[M, ({type E[X, Y[_]] = WriterT[X, Y, W]})#E] {
+      override def lift[A](a: M[A]): WriterT[A, M, W] = WriterT.lift(a)
+    }
+
+  def tell[M[_], W](a:W)(implicit f:Functor[M], m:Monad[M], w:Monoid[W]):WriterT[Unit, M, W] = WriterT {
+    m.pure { Writer.tell(a)}
+  }
 }
--- src/test/scala/learnfp/transformer/WriterTTest.scala
+++ src/test/scala/learnfp/transformer/WriterTTest.scala
@@ -1,5 +1,60 @@
 package learnfp.transformer
 
-class WriterTTest {
+import org.scalatest.{Matchers, WordSpecLike}
+import learnfp.functor.FunctorOps._
+import learnfp.functor.Writer
+import learnfp.monad.MonadOps.toMonadOpsPure
+import learnfp.monoid.ListMonoid._
+import learnfp.transformer.WriterT._
 
+class WriterTTest extends WordSpecLike with Matchers {
+  "WriterT" should {
+    import learnfp.functor.Id
+    import learnfp.functor.IdInstance._
+    import learnfp.monad.IdInstance._
+
+    "work with Id" in {
+      type App[A] = WriterT[A, Id, List[String]];
+      {
+        for {
+          x <- 10.pure[App]
+          _ <- WriterT.tell(List("1"))
+          y <- 20.pure[App]
+          _ <- WriterT.tell(List("2"))
+          z <- 30.pure[App]
+        } yield {(x, y, z)}
+      }.runWriterT.value.run() shouldBe (List("1", "2"), (10, 20, 30))
+    }
+
+    import learnfp.functor.Maybe._
+    import learnfp.functor.MaybeInstance._
+    import learnfp.monad.MaybeInstance._
+
+    "work with maybe just" in {
+      type App[A] = WriterT[A, Maybe, List[String]];
+      {
+        for {
+          x <- 10.pure[App]
+          _ <- WriterT.tell[Maybe, List[String]](List("1"))
+          y <- 20.pure[App]
+          _ <- WriterT.tell[Maybe, List[String]](List("2"))
+          z <- 30.pure[App]
+        } yield {(x, y, z)}
+      }.runWriterT.asInstanceOf[Just[Writer[List[String], (Int, Int, Int)]]].value.run() shouldBe (List("1", "2"), (10, 20, 30))
+    }
+
+    "work with maybe nothing" in {
+      type App[A] = WriterT[A, Maybe, List[String]];
+      {
+        for {
+          x <- 10.pure[App]
+          _ <- WriterT.tell[Maybe, List[String]](List("1"))
+          y <- 20.pure[App]
+          _ <- WriterT.tell[Maybe, List[String]](List("2"))
+          _ <- WriterT.lift[Unit, Maybe, List[String]](nothing[Unit]())
+          z <- 30.pure[App]
+        } yield {(x, y, z)}
+      }.runWriterT.asInstanceOf[Nothing[Unit]] shouldBe nothing[Unit]()
+    }
+  }
 }
