493335b6a69fc50d43cb4d95dcafd51f70a1a8bf
Yuriy Netesov <yuriy.netesov@gmail.com>
2017-12-24 13:39:21 +0100

    StateT

--- src/test/scala/learnfp/transformer/StateTTest.scala
+++ src/test/scala/learnfp/transformer/StateTTest.scala
@@ -1,6 +1,6 @@
 package learnfp.transformer
 
-import org.scalatest.{WordSpecLike, Matchers}
+import org.scalatest.{Matchers, WordSpecLike}
 
 //import learnfp.functor.FunctorOps.
 import learnfp.monad.MonadOps.toMonadOpsPure
@@ -59,6 +59,31 @@ class StateTTest extends WordSpecLike with Matchers {
           _ <- putT[String, Maybe]("drie")
         } yield { (x, y, z) }
       }.runStateT("null") shouldBe nothing[Unit]()
+    }
+
+    import learnfp.functor.Writer
+    import learnfp.functor.Writer._
+    import learnfp.functor.WriterInstance._
+    import learnfp.monad.WriterInstance._
+
+    import learnfp.monoid.ListMonoid._
+
+    "work with Writer" in {
+      type StringWriter[A] = Writer[List[String], A];
+      type App[A] = StateT[String, StringWriter, A];
+      {
+        for {
+          x <- 10.pure[App]
+          _ <- putT[String, StringWriter]("een")
+          _ <- StateT.lift[String, StringWriter, Unit](Writer.tell[List[String]](List("one")))
+          y <- 20.pure[App]
+          _ <- putT[String, StringWriter]("twee")
+          _ <- StateT.lift[String, StringWriter, Unit](Writer.tell[List[String]](List("two")))
+          z <- 30.pure[App]
+          _ <- putT[String, StringWriter]("drie")
+          _ <- StateT.lift[String, StringWriter, Unit](Writer.tell[List[String]](List("three")))
+        } yield { (x, y, z) }
+      }.runStateT("null").run() shouldBe (List("one", "two", "three"), ("drie", (10, 20, 30)))
     }
   }
 }
