487e9031f78c679a0e5ab870ad7931a5ffd4ac82
ynetesov <yuriy.netesov@gmail.com>
2018-01-05 22:30:06 +0100

    foldable

--- src/main/scala/learnfp/foldable/Foldable.scala
+++ src/main/scala/learnfp/foldable/Foldable.scala
@@ -1,5 +1,8 @@
 package learnfp.foldable
 
+import learnfp.functor.Disjunction.{Disjunction, LeftDisjunction, RightDisjunction}
+import learnfp.functor.Maybe.{Just, Maybe, Nothing}
+
 trait Foldable[C[_]] {
   def foldr[A, B](xs:C[A])(init:B)(fx:(A, B) => B):B
 }
@@ -9,17 +12,29 @@ object FoldableInstances {
     override def foldr[A, B](xs: List[A])(init: B)(fx: (A, B) => B): B = xs.foldRight(init)(fx)
   }
 
-  implicit def tuple2Foldable = new Foldable[({type E[X] = (X, X)})#E] {
+  implicit def tuple2Foldable = new Foldable[({type E[X] = Tuple2[X, X]})#E] {
     override def foldr[A, B](xs: (A, A))(init: B)(fx: (A, B) => B): B = {
-      val fst = fx(xs._1, init)
-      val snd = fx(xs._2, fst)
-      snd
+      fx(xs._1, fx(xs._2, init))
     }
   }
 
   implicit def tuple3Foldable = new Foldable[({type E[X] = (X, X, X)})#E] {
     override def foldr[A, B](xs: (A, A, A))(init: B)(fx: (A, B) => B): B = {
-      List(xs._1, xs._2, xs._3).foldRight(init)(fx) // super lazy...
+      fx(xs._1, fx(xs._2, fx(xs._3, init)))
+    }
+  }
+
+  implicit val maybeFoldable = new Foldable[Maybe] {
+    override def foldr[A, B](xs: Maybe[A])(init: B)(fx: (A, B) => B): B = xs match {
+      case Just(x) => fx(x, init)
+      case Nothing() => init
+    }
+  }
+
+  implicit def disjunctionFoldable[L] = new Foldable[({type E[X] = Disjunction[L, X]})#E] {
+    override def foldr[A, B](xs: Disjunction[L, A])(init: B)(fx: (A, B) => B): B = xs match {
+      case RightDisjunction(x) => fx(x, init)
+      case LeftDisjunction(_) => init
     }
   }
 }
@@ -30,4 +45,9 @@ class FoldableOps[C[_], A](xs:C[A])(implicit foldable: Foldable[C]) {
 
 object FoldableOps {
   implicit def toFoldableOps[C[_], A](xs:C[A])(implicit foldable: Foldable[C]) = new FoldableOps[C, A](xs)
+  implicit def tuple2ToFoldableOps[A](xs:(A, A))(implicit foldable:Foldable[({type E[X] = (X, X)})#E]) = new FoldableOps[({type E[X] = (X, X)})#E, A](xs)
+  implicit def tuple3ToFoldableOps[A](xs:(A, A, A))(implicit foldable:Foldable[({type E[X] = (X, X, X)})#E]) = new FoldableOps[({type E[X] = (X, X, X)})#E, A](xs)
+  implicit def disjunctionToFoldableOps[L, A](xs:Disjunction[L, A])(implicit foldable:Foldable[({type E[X] = Disjunction[L, X]})#E]) =
+    new FoldableOps[({type E[X] = Disjunction[L, X]})#E, A](xs)
+
 }
--- src/test/scala/learnfp/foldable/FoldableTest.scala
+++ src/test/scala/learnfp/foldable/FoldableTest.scala
@@ -12,7 +12,20 @@ class FoldableTest extends WordSpecLike with Matchers {
     }
 
     "work with tuples" in {
-      toFoldableOps(("a", "b")).myfoldr("init"){(x, a) => a + x} shouldBe ("initba")
+      ("a", "b").myfoldr("init"){(x, a) => a + x} shouldBe ("initba")
+      ("a", "b", "c").myfoldr("init"){(x, a) => a + x} shouldBe ("initcba")
+    }
+
+    import learnfp.functor.Maybe._
+    "work with Maybe" in {
+      just(1).myfoldr(0) {(x, a) => a + x } shouldBe 1
+      nothing[Int]().myfoldr(0) {(x, a) => a + x } shouldBe 0
+    }
+
+    import learnfp.functor.Disjunction._
+    "work with disjunction" in {
+      right[String, String]("abc").myfoldr("init"){(x, a) => a + x} shouldBe "initabc"
+      left[String, Int]("foo").myfoldr("init"){(x, a) => a + x} shouldBe "init"
     }
   }
 }
