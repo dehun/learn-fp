0ef31d9cf25f245b73e60122d9052a1b37e79669
Fernando <nandotorterolo@gmail.com>
2018-01-06 19:09:56 -0300

    val f not used

--- src/test/scala/learnfp/functor/IdTest.scala
+++ src/test/scala/learnfp/functor/IdTest.scala
@@ -8,14 +8,14 @@ class IdTest extends WordSpecLike with Matchers {
 
   "id" should {
     "work on simple functions" in {
-      val  f = implicitly[Functor[Id]]
       Id(1) fmap {x:Int => x + 1} shouldBe Id(2)
       Id("one, ") fmap {s => s + "two"} shouldBe Id("one, two")
-
     }
+
     "obey identity" in {
       Id(1) fmap identity shouldBe Id(1)
     }
+    
     "obey composition" in {
       val f = {x:Int => x + 1}
       val g = {x:Int => x * 2}
