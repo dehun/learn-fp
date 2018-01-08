9babc4ce09d0909bcf57847f6f4d26c6bc38f62b
ynetesov <yuriy.netesov@gmail.com>
2018-01-08 09:16:24 +0100

    wipe answers for foldable

--- src/main/scala/learnfp/foldable/Foldable.scala
+++ src/main/scala/learnfp/foldable/Foldable.scala
@@ -10,37 +10,27 @@ trait Foldable[C[_]] {
 
 object FoldableInstances {
   implicit def idFoldable = new Foldable[Id] {
-    override def foldr[A, B](xs: Id[A])(init: B)(fx: (A, B) => B): B = fx(xs.value, init)
+    override def foldr[A, B](xs: Id[A])(init: B)(fx: (A, B) => B): B = ???
   }
 
   implicit def listFoldable = new Foldable[List] {
-    override def foldr[A, B](xs: List[A])(init: B)(fx: (A, B) => B): B = xs.foldRight(init)(fx)
+    override def foldr[A, B](xs: List[A])(init: B)(fx: (A, B) => B): B = ???
   }
 
   implicit def tuple2Foldable = new Foldable[({type E[X] = Tuple2[X, X]})#E] {
-    override def foldr[A, B](xs: (A, A))(init: B)(fx: (A, B) => B): B = {
-      fx(xs._1, fx(xs._2, init))
-    }
+    override def foldr[A, B](xs: (A, A))(init: B)(fx: (A, B) => B): B = ???
   }
 
   implicit def tuple3Foldable = new Foldable[({type E[X] = (X, X, X)})#E] {
-    override def foldr[A, B](xs: (A, A, A))(init: B)(fx: (A, B) => B): B = {
-      fx(xs._1, fx(xs._2, fx(xs._3, init)))
-    }
+    override def foldr[A, B](xs: (A, A, A))(init: B)(fx: (A, B) => B): B = ???
   }
 
   implicit val maybeFoldable = new Foldable[Maybe] {
-    override def foldr[A, B](xs: Maybe[A])(init: B)(fx: (A, B) => B): B = xs match {
-      case Just(x) => fx(x, init)
-      case Nothing() => init
-    }
+    override def foldr[A, B](xs: Maybe[A])(init: B)(fx: (A, B) => B): B = ???
   }
 
   implicit def disjunctionFoldable[L] = new Foldable[({type E[X] = Disjunction[L, X]})#E] {
-    override def foldr[A, B](xs: Disjunction[L, A])(init: B)(fx: (A, B) => B): B = xs match {
-      case RightDisjunction(x) => fx(x, init)
-      case LeftDisjunction(_) => init
-    }
+    override def foldr[A, B](xs: Disjunction[L, A])(init: B)(fx: (A, B) => B): B = ???
   }
 }
 
@@ -54,5 +44,4 @@ object FoldableOps {
   implicit def tuple3ToFoldableOps[A](xs:(A, A, A))(implicit foldable:Foldable[({type E[X] = (X, X, X)})#E]) = new FoldableOps[({type E[X] = (X, X, X)})#E, A](xs)
   implicit def disjunctionToFoldableOps[L, A](xs:Disjunction[L, A])(implicit foldable:Foldable[({type E[X] = Disjunction[L, X]})#E]) =
     new FoldableOps[({type E[X] = Disjunction[L, X]})#E, A](xs)
-
 }
