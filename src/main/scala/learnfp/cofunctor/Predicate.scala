3fc53f527570139dcd280f694cc890b7ceb3c106
ynetesov <yuriy.netesov@gmail.com>
2018-01-09 21:05:59 +0100

    add show

--- src/main/scala/learnfp/cofunctor/Predicate.scala
+++ src/main/scala/learnfp/cofunctor/Predicate.scala
@@ -4,8 +4,6 @@ case class Predicate[A](getPredicate:A => Boolean)
 
 object Predicate {
   implicit val predicateCoFunctorInstance = new CoFunctor[Predicate] {
-    override def cmap[A, B](b: Predicate[B])(fx: A => B): Predicate[A] = Predicate { a:A =>
-      b.getPredicate(fx(a))
-    }
+    override def cmap[A, B](b: Predicate[B])(fx: A => B): Predicate[A] = Predicate { a:A => b.getPredicate(fx(a)) }
   }
 }
