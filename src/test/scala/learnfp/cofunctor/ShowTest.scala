846a64b61187ac535367fe57dcef956c2c1c5aae
ynetesov <yuriy.netesov@gmail.com>
2018-01-10 09:10:28 +0100

    show tests

---
+++ src/test/scala/learnfp/cofunctor/ShowTest.scala
@@ -0,0 +1,25 @@
+package learnfp.cofunctor
+
+import learnfp.cofunctor.CoFunctorOps._
+import org.scalatest.{Matchers, WordSpecLike}
+
+class ShowTest extends WordSpecLike with Matchers {
+  "Show" should {
+    "work" in {
+      { Show {x:Int => x.toString} cmap
+        {f:Double => f.floor.toInt} cmap
+        {z:String => z.toDouble} }.show("3.14") shouldBe "3"
+    }
+    "obey identity" in {
+      { Show {x:Int => x.toString} cmap identity[Int] }.show(3) shouldBe "3"
+    }
+    "obey composition" in {
+      val f = { x:Double => x.floor.toInt }
+      val g = { x:String => x.reverse.toDouble }
+      val intShow = Show {x:Int => x.toString}
+      val lshow = intShow cmap f cmap g
+      val rshow = intShow cmap {x:String => f(g(x))}
+      lshow.show("123.123") shouldBe rshow.show("123.123")
+    }
+  }
+}
