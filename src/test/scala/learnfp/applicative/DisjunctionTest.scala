65cfa3c7c3fdb881240de2d2f1a39f216f01a232
Lukasz Drygala <lukasz.drygala@ocado.com>
2018-01-17 21:29:52 +0100

    cleanup DisjunctionTest for Applicative

--- src/test/scala/learnfp/applicative/DisjunctionTest.scala
+++ src/test/scala/learnfp/applicative/DisjunctionTest.scala
@@ -8,14 +8,14 @@ class DisjunctionTest extends WordSpecLike with Matchers {
   import learnfp.applicative.DisjunctionInstance._
 
   "disjunction applicative" should {
-    def rght(x:Int):Disjunction[String, Int] = right[String, Int](x);
+    def right(x:Int):Disjunction[String, Int] = RightDisjunction(x)
 
     "work with right" in {
-      {(x:Int, y:Int, z:Int) => x + y + z}.curried `<$>` rght(10) <*> rght(20) <*> rght(30) shouldBe rght(10 + 20 + 30)
+      {(x:Int, y:Int, z:Int) => x + y + z}.curried `<$>` right(10) <*> right(20) <*> right(30) shouldBe right(10 + 20 + 30)
     }
 
     "work with left" in {
-      {(x:Int, y:Int, z:Int) => x + y + z}.curried `<$>` rght(10) <*> left[String, Int]("boom") <*> rght(30) shouldBe left[String, Int]("boom")
+      {(x:Int, y:Int, z:Int) => x + y + z}.curried `<$>` right(10) <*> left[String, Int]("boom") <*> right(30) shouldBe left[String, Int]("boom")
     }
   }
 }
