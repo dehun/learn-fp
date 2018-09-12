7882f768b3ad23d021a02ce7e3a51a3dd4903506
ynetesov <yuriy.netesov@gmail.com>
2018-09-12 18:38:38 +0200

    merge

--- src/main/scala/learnfp/traversable/Traversable.scala
+++ src/main/scala/learnfp/traversable/Traversable.scala
@@ -15,29 +15,19 @@ object TraversableInstances {
   import learnfp.functor.FunctorOps._
 
   implicit val idTraversableInstance = new Traversable[Id] {
-    override def traverse[A, B, F[_]](xs: Id[A])(fx:A => F[B])(implicit foldable: Foldable[Id], functor:Functor[F], applicative: Applicative[F]): F[Id[B]] = {
-      fx(xs.value).fmap(b => Id(b))
-    }
+    override def traverse[A, B, F[_]](xs: Id[A])(fx:A => F[B])(implicit foldable: Foldable[Id], functor:Functor[F], applicative: Applicative[F]): F[Id[B]] = ???
   }
 
   type STuple3[A] = (A, A, A)
   def stuple3[A](a:A, b:A, c:A):STuple3[A] = (a, b, c)
 
   implicit val tuple3TraversableInstance = new Traversable[STuple3] {
-    override def traverse[A, B, F[_]](xs: (A, A, A))(fx: A => F[B])(implicit foldable: Foldable[STuple3], functor: Functor[F], applicative: Applicative[F]): F[(B, B, B)] = {
-      import learnfp.applicative.ApplicativeOps._
-      { (a:B, b:B, c:B)  => (a, b, c) }.curried `<$>` fx(xs._1) `<*>` fx(xs._2) `<*>` fx(xs._3)
-    }
+    override def traverse[A, B, F[_]](xs: (A, A, A))(fx: A => F[B])(implicit foldable: Foldable[STuple3], functor: Functor[F], applicative: Applicative[F]): F[(B, B, B)] = ???
   }
 
   implicit val listTraversableInstance = new Traversable[List] {
     override def traverse[A, B, F[_]](xs: List[A])(fx: A => F[B])(implicit foldable: Foldable[List],
-                                                                  functor: Functor[F], applicative: Applicative[F]): F[List[B]] = {
-      import learnfp.applicative.ApplicativeOps._
-      foldable.foldr(xs)(applicative.pure(List.empty[B])) { (a:A, b:F[List[B]]) =>
-        { (l:B, r:List[B]) => l::r}.curried `<$>` fx(a) <*> b
-      }
-    }
+                                                                  functor: Functor[F], applicative: Applicative[F]): F[List[B]] = ???
   }
 }
 
@@ -51,7 +41,7 @@ object TraversableOps {
 
 class SequenceOps[A, C[_], F[_]](xs:C[F[A]])(implicit foldable: Foldable[C], traversable: Traversable[C],
                                              functor: Functor[F], applicative: Applicative[F]) {
-  def sequence:F[C[A]] = traversable.traverse(xs)(identity)
+  def sequence:F[C[A]] = ???
 }
 
 object SequenceOps {
